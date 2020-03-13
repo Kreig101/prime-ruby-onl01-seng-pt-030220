6534e0031eef49ed2ace3e3b8335521b66f3bd28
Craig Storer <craigstorer@gmail.com>
2020-03-13 21:47:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,32 +3,10 @@ require 'pry'
 
 
 def prime?(num)
-# if !num.integer?
-#   false
- if num < 2 
+if num < 2 
   false
-# elsif num == 2
-#   true
  else  (2..num-1).to_a.all? {|x| num % x != 0}
-# else
-#   true
-
 end
 end
 
-#     last = num.to_i 
-# while (num <= last)
-#     prime_flag = true
-# x = 2
- 
-# while num <= / 2
-    
-    # if 
-#       prime_flag = flase
-#     break
-#     end
-#       x += 1
-# end
-# end
-# end
 
