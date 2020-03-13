8874735421b629f9f87dbdedeeee1ea41f367d21
Craig Storer <craigstorer@gmail.com>
2020-03-13 21:47:55 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -3,31 +3,10 @@ require 'pry'
 
 
 def prime?(num)
-# num.split.each do |x|
- if !num.interger?
+if num < 2 
   false
- elsif num < 2 
-  false
- elsif num == 2
-  true
-# elsif num.split.each {|x| return false if num % == 0}
-
+ else  (2..num-1).to_a.all? {|x| num % x != 0}
 end
 end
 
-#     last = num.to_i 
-# while (num <= last)
-#     prime_flag = true
-# x = 2
- 
-# while num <= / 2
-    
-    # if 
-#       prime_flag = flase
-#     break
-#     end
-#       x += 1
-# end
-# end
-# end
 
